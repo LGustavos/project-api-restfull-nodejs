e1f5c1cd19e6e9d86832c6271e1536c7bd41cbe1
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-03-18 00:17:16 -0300

    First commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+module.exports = {
+  "username": process.env.USERNAME,
+  "password":process.env.PASSWORD,
+  "database": process.env.DATABASE,
+  "host": process.env.HOST,
+  "dialect": "mysql",
+  "define": {
+    "timestamps": true,
+    "underscored": true
+  }
+}
